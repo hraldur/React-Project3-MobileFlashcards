7cca39cec04a18b4c55ebfb920c8762b1cb968ff
Hrafnhildur <hraldur28@gmail.com>
2018-04-30 14:36:11 +0200

    add AsyncStorage

---
+++ utils/api.js
@@ -0,0 +1,41 @@
+import { AsyncStorage } from "react-native";
+import { DECK_STORAGE_KEY, formatDeckResults } from './_deck'
+
+export function getDecks () {
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then(formatDeckResults)
+}
+
+export function getDeck (id) {
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      return data[id]
+    })
+}
+
+export function saveDeckTitle (title) {
+  console.log(title)
+  return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
+  [title]: {
+     title: title,
+     questions: []
+   }
+  }))
+}
+
+export function addCardToDeck (title, card) {
+  console.log("API!!", title, card)
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      data[title].questions.push(card)
+      AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data))
+  })
+}
+
+
+export function fetchDeckResults() {
+	return AsyncStorage.getItem(DECK_STORAGE_KEY)
+		.then(formatDeckResults)
+}
---
+++ utils/colors.js
@@ -0,0 +1,4 @@
+export const gray = '#757575'
+export const white = '#fff'
+export const blue = '#18CBB0'
+export const yellow = '#EAD720'
